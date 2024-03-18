ec971f34aead18cafbfec9cefa305ff9552efc62
Bryant-Wang24 <485969746@qq.com>
2024-03-18 15:00:28 +0800

    feat: cors

--- server/server.go
+++ server/server.go
@@ -1,12 +1,23 @@
 package server
 
 import (
+	"time"
+
 	"example.com/gin_forum/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RunHTTPServer() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	handler.AddUserHandler(r)
 	handler.AddArticleHandler(r)
 	handler.AddTagsHandler(r)
